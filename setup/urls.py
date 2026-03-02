dceb6d8ff64f50b75f20c5883e9e335289aac786
guilherme <270210.gui@gmail.com>
2026-03-01 23:04:03 -0300

    Add URL routing for api_usuario app and initial view implementations

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,10 @@
 #Roteamento principal (inclui as rotas de 'api')
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# Onde o projeto deve procurar para as URLs da API
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('api_usuario.urls')),
 ]
