c3a1dee42cfad1b1dbac061255590f4d8cf339c3
guilherme <270210.gui@gmail.com>
2026-03-23 23:47:59 -0300

    feat: add TokenVerificacaoEmail model for email verification tokens

--- api_usuario/models.py
+++ api_usuario/models.py
@@ -14,8 +14,15 @@ class Usuario(models.Model):
 class Credencial(models.Model):
     email = models.EmailField(max_length=50, unique=True)
     senha = models.CharField(max_length=255) # Aumentado para suportar o hash do bcrypt
-    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='credencial', db_column='id_usuarios')
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='credencial', db_column='id_usuarios') # Chave estrangeira para o usuário
 
     class Meta:
         db_table = 'credenciais' # Força o nome da tabela no banco
 
+class TokenVerificacaoEmail(models.Model):
+    token = models.CharField(max_length=255, unique=True)
+    verificado = models.BooleanField(default=False)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='tokens_verificacao_email', db_column='id_usuarios') # Chave estrangeira para o usuário
+
+    class Meta:
+        db_table = 'tokens_verificacao_email' # Força o nome da tabela no banco
