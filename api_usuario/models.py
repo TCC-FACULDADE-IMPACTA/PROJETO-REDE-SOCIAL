68b43078bfcf503a9109a7de4c8b0afa5229f8ed
guilherme <270210.gui@gmail.com>
2026-03-23 23:54:10 -0300

    feat: add Postagem model for user posts with GIF and description fields

--- api_usuario/models.py
+++ api_usuario/models.py
@@ -26,3 +26,13 @@ class TokenVerificacaoEmail(models.Model):
 
     class Meta:
         db_table = 'tokens_verificacao_email' # Força o nome da tabela no banco
+
+
+
+class Postagem(models.Model):
+    gif = models.BinaryField()
+    descricao = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='postagens', db_column='id_usuarios') # Chave estrangeira para o usuário
+
+    class Meta:
+        db_table = 'postagens' # Força o nome da tabela no banco
