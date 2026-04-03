5fc124485dd2834bf32555f5be4974d9fd15bb23
guilherme <270210.gui@gmail.com>
2026-04-03 15:16:58 -0300

    feat: update PostSentimento model with gif_url and texto_sentimento fields; add __str__ method for better representation; create tests for api_postagem and api_usuario

--- api_usuario/models.py
+++ api_usuario/models.py
@@ -19,6 +19,9 @@ class Credencial(models.Model):
     class Meta:
         db_table = 'credenciais' # Força o nome da tabela no banco
 
+    def __str__(self): # Representação em string do objeto
+        return self.email
+
 class TokenVerificacaoEmail(models.Model):
     token = models.CharField(max_length=255, unique=True)
     verificado = models.BooleanField(default=False)
