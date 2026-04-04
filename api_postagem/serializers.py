1bd6642ac2bc61954281a585bc0870bba7090593
guilherme <270210.gui@gmail.com>
2026-04-04 20:25:11 -0300

    feat: update models and serializers to include additional user fields and improve admin interface

--- api_postagem/serializers.py
+++ api_postagem/serializers.py
@@ -3,10 +3,23 @@ from .models import PostSentimento
 
 # Serializer para o frontend criar um post com texto e GIF
 class CriaPostSentimentoSerializer(serializers.ModelSerializer):
+    usuario_nome = serializers.ReadOnlyField(source='usuario.nome', read_only=True)  # Exibe o nome do usuário no frontend
+    usuario_username = serializers.ReadOnlyField(source='usuario.username', read_only=True)  # Exibe o username do usuário no frontend
+    # usuario_foto = serializers.ImageField(source='usuario.foto', read_only=True)  # Exibe a foto do usuário no frontend AGUARDANDO PERFIL DE USUARIO PARA USO...
+
     class Meta:
         model = PostSentimento
-        fields = ['gif_url', 'texto_sentimento']
+        fields = [
+            'id',
+            'usuario_nome',
+            'usuario_username',
+            'texto_sentimento',
+            'gif_url',
+            'data_criacao',
+        ]  # Campos que serão enviados para o frontend
+
         extra_kwargs = {
             'texto_sentimento': {'required': True, 'allow_blank': False}, # Campo obrigatório
-            'gif_url': {'required': False, 'allow_blank': True} # Campo opcional
+            'gif_url': {'required': False, 'allow_blank': True}, # Campo opcional
+            'usuario': {'required': True}  # Campo obrigatório
         }
--- api_usuario/admin.py
+++ api_usuario/admin.py
@@ -3,8 +3,12 @@
 from django.contrib import admin
 from .models import Usuario, Credencial
 
-admin.site.register(Usuario)
-admin.site.register(Credencial)
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'username', 'nascimento')
+    search_fields = ('nome', 'username')
 
-
-
+@admin.register(Credencial)
+class CredencialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'usuario')
+    search_fields = ('email',)
