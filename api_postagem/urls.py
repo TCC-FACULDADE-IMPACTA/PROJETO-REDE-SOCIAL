4d3bcaa2c3f1ad8d088d3dcaac701dae40540e22
guilherme <270210.gui@gmail.com>
2026-04-07 02:23:28 -0300

    feat: implement atualizar_postagem endpoint with authentication and validation checks

--- api_postagem/urls.py
+++ api_postagem/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/criar_post/', views.criar_post, name='criar_post'),
     path('api/deletar_postagem/<int:post_id>/', views.deletar_postagem, name='deletar_postagem'),
     path('api/listar_postagens/', views.listar_postagens, name='listar_postagens'),
+    path('api/atualizar_postagem/<int:post_id>/', views.atualizar_postagem, name='atualizar_postagem')
 ]
