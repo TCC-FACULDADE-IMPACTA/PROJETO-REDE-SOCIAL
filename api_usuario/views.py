d6cc3a567e08d9ca2611708e712a537e74adf233
guilherme <270210.gui@gmail.com>
2026-04-23 23:40:26 -0300

    feat: simplify authentication flow by utilizing token decorator in user profile and photo upload views

--- api_usuario/views.py
+++ api_usuario/views.py
@@ -9,6 +9,7 @@ from django.conf import settings
 from datetime import datetime, timedelta, timezone
 import jwt
 from rest_framework.parsers import MultiPartParser, FormParser
+from utils.autenticacao import token_obrigatorio
 
 # SE O MÉTODO FOR POST, PROCESSA O CADASTRO
 @api_view(['POST'])
@@ -98,50 +99,23 @@ def efetuar_login(request):
 
 # FLUXO: VER PERFIL DO USUÁRIO
 @api_view(['GET'])
+@token_obrigatorio
 def ver_perfil(request):
     """ Fluxo: VER PERFIL DO USUÁRIO """
-    
-    auth_header = request.headers.get('Authorization')
-    # VERIFICA SE O CABEÇALHO DE AUTORIZAÇÃO ESTÁ PRESENTE E COMEÇA COM 'Bearer '
-    if not auth_header or not auth_header.startswith('Bearer '):
-        return Response({'erro': 'Token de autenticação não fornecido.'}, status=status.HTTP_401_UNAUTHORIZED)
-    # EXTRAINDO O TOKEN DO CABEÇALHO
-    token = auth_header.split(' ')[1]
 
-    # DECODIFICANDO O TOKEN PARA OBTENÇÃO DOS DADOS DO USUÁRIO
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-        usuario_id = payload.get('usuario_id')
-        usuario = Usuario.objects.get(id=usuario_id)
-    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, Usuario.DoesNotExist):
-        return Response({'erro': 'Token inválido ou expirado.'}, status=status.HTTP_401_UNAUTHORIZED)
+    usuario = request.usuario
 
     # SE O USUÁRIO FOR ENCONTRADO, RETORNA OS DADOS DO PERFIL
     serializer = PerfilSerializer(usuario)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
+@token_obrigatorio
 @parser_classes([MultiPartParser, FormParser])
 def upload_foto(request):
     """ Fluxo: UPLOAD DE FOTO DE PERFIL """
 
-    #testar porque no postman não está funcionando a injeção de arquivos
-    # print(f"FILES recebidos: {request.FILES.keys()}") # Deve imprimir dict_keys(['foto'])
-    # print(f"DATA recebido: {request.data}")
-
-    # AUTENTICAÇÃO DO USUÁRIO (MESMO FLUXO DE VER PERFIL)
-    auth_header = request.headers.get('Authorization')
-    if not auth_header or not auth_header.startswith('Bearer '):
-        return Response({'erro': 'Token não fornecido.'}, status=status.HTTP_401_UNAUTHORIZED)
-
-    token = auth_header.split(' ')[1]
-
-    try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-        usuario_id = payload.get('usuario_id')
-        usuario = Usuario.objects.get(id=usuario_id)
-    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, Usuario.DoesNotExist):
-        return Response({'erro': 'Token inválido ou expirado.'}, status=status.HTTP_401_UNAUTHORIZED)
+    usuario = request.usuario
 
     # VERIFICA SE O ARQUIVO 'foto' ESTÁ PRESENTE NOS FILES ENVIADOS
     if 'foto' not in request.FILES:
